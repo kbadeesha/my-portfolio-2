eeda4fe11b01945b7835baf6dd1b8586b1126688
Adeesha Shashadara <adeesha.kristhorubaduge@gmail.com>
2025-11-09 17:13:44 +0530

    Linting added

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,31 +4,26 @@ module.exports = {
     browser: true,
     es2020: true,
   },
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
   settings: {
     react: {
-      version: 'detect',
+      version: "detect",
     },
   },
-  plugins: [
-    'react',
-    'react-hooks',
-    'react-refresh',
-    '@typescript-eslint',
-  ],
+  plugins: ["react", "react-hooks", "react-refresh", "@typescript-eslint"],
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:@typescript-eslint/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
   ],
   rules: {
-    'react/react-in-jsx-scope': 'off', // Not needed in Vite/React 17+
-    'react-refresh/only-export-components': 'warn',
-    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    "react/react-in-jsx-scope": "off", // Not needed in Vite/React 17+
+    "react-refresh/only-export-components": "warn",
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
   },
-}
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import { Link, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Hero from './pages/Hero/Hero'
-import Projects from './pages/Projects/Projects'
+import { Link, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Hero from "./pages/Hero/Hero";
+import Projects from "./pages/Projects/Projects";
 
 function App() {
-
-return (
+  return (
     <div>
       <nav>
-        <Link to="/">Hero</Link> |{" "}
-        <Link to="/about">Projects</Link> |{" "}
+        <Link to="/">Hero</Link> | <Link to="/about">Projects</Link> |{" "}
       </nav>
 
       <Routes>
@@ -17,7 +15,7 @@ return (
         <Route path="/about" element={<Projects />} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
