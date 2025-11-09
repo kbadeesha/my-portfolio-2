fbf4de737c52d3d9b00a989267b15d195c622c81
Adeesha Shashadara <adeesha.kristhorubaduge@gmail.com>
2025-11-09 16:52:02 +0530

    Linting added

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+    'react-refresh',
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off', // Not needed in Vite/React 17+
+    'react-refresh/only-export-components': 'warn',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+  },
+}
