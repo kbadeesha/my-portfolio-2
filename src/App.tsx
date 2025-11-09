fafa1d0227035fc236419014e126dae1d51e23a1
Adeesha Shashadara <adeesha.kristhorubaduge@gmail.com>
2025-11-09 16:39:31 +0530

    Initialized  Router Dom and Routs

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import { Link, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Hero from './pages/Hero/Hero'
+import Projects from './pages/Projects/Projects'
 
 function App() {
 
-  return (
-    <>
-      <h1>Heading 1</h1>
-    </>
+return (
+    <div>
+      <nav>
+        <Link to="/">Hero</Link> |{" "}
+        <Link to="/about">Projects</Link> |{" "}
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/about" element={<Projects />} />
+      </Routes>
+    </div>
   )
 }
 
