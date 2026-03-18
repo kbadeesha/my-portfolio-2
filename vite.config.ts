46d3bdcb57f98c8e461f5a4846d5ffa7fa7b460e
Adeesha Shashadara <adeesha.kristhorubaduge@gmail.com>
2026-03-18 16:26:55 -0400

    Vecel.json

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // This is the key setting for SPAs. 
+  // It tells Vite to always serve index.html for non-file requests.
+  appType: 'spa',
 });
